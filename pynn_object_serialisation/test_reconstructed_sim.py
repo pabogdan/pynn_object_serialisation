fb83c813b1ddf1bedf87d379491c826bd6e8d901
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2019-06-06 10:53:50 +0100

    keeping track of errors too

--- pynn_object_serialisation/test_reconstructed_sim.py
+++ pynn_object_serialisation/test_reconstructed_sim.py
@@ -12,12 +12,17 @@ from pynn_object_serialisation.functions import \
     restore_simulator_from_file
 import os
 import numpy as np
+import traceback
 start_time = plt.datetime.datetime.now()
 runtime = args.sim_time
+e = None
 model_file_path = os.path.join(args.dir, args.model)
 populations, projections = restore_simulator_from_file(
     sim, model_file_path, prune_level=args.prune_level)
-sim.run(runtime)
+try:
+    sim.run(runtime)
+except Exception as e:
+    traceback.print_exc()
 sim.end()
 end_time = plt.datetime.datetime.now()
 total_time = end_time - start_time
@@ -28,6 +33,7 @@ filename += "_pruned_at_" + str(args.prune_level)
 np.savez_compressed(filename,
                     total_time=total_time,
                     model_file_path=model_file_path,
-                    prune_level=args.prune_level)
+                    prune_level=args.prune_level,
+                    exception=str(e))
 if __name__ == "__main__":
     pass
