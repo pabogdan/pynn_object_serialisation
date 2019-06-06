39e020108748031fbbb39cd385943acef4c44006
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2019-06-06 10:48:36 +0100

    saving some info for future analysis

--- pynn_object_serialisation/test_reconstructed_sim.py
+++ pynn_object_serialisation/test_reconstructed_sim.py
@@ -11,6 +11,7 @@ import pylab as plt
 from pynn_object_serialisation.functions import \
     restore_simulator_from_file
 import os
+import numpy as np
 start_time = plt.datetime.datetime.now()
 runtime = args.sim_time
 model_file_path = os.path.join(args.dir, args.model)
@@ -21,5 +22,12 @@ sim.end()
 end_time = plt.datetime.datetime.now()
 total_time = end_time - start_time
 print("Total time elapsed -- " + str(total_time))
+suffix = end_time.strftime("_%H%M%S_%d%m%Y")
+filename = "reconstruction_results"
+filename += "_pruned_at_" + str(args.prune_level)
+np.savez_compressed(filename,
+                    total_time=total_time,
+                    model_file_path=model_file_path,
+                    prune_level=args.prune_level)
 if __name__ == "__main__":
     pass
