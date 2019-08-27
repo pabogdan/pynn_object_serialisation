9d6a9286949940e4c2e4a16c5087abf535553367
ej159 <edwardgeorgejones@gmail.com>
2019-08-27 16:42:51 +0100

    fixed many functions of ODP

--- pynn_object_serialisation/OutputDataProcessor.py
+++ pynn_object_serialisation/OutputDataProcessor.py
@@ -1,32 +1,33 @@
+import numpy as np
+
 class OutputDataProcessor():
     ''' A class to represent the output of a serialised model and to
     alllow for easier processing.
     '''
-    path = 'results/output_t_stim_2000_testing_examples_1.npz'
 
+    
     def __init__(self, path):
         self.data = np.load(path)
-        self.input_layer_name = 'InputLayer'
-        self.output_layer_name = self.get_ordered_layer_names_from_data(
-            self.data)[-1]
-        self.y_test = self.data['y_test']
         self.spikes_dict = self.reconstruct_spikes_dict()
-        self.t_stim = data['t_stim']
+        self.layer_names = list(self.spikes_dict.keys())
+        self.order_layer_names()
+        self.input_layer_name = 'InputLayer'
+        self.output_layer_name = self.layer_names[-1]
+        self.y_test = self.data['y_test']
+        self.t_stim = self.data['t_stim']
         self.runtime = int(self.data['runtime'])
         self.N_layer = int(self.data['N_layer'])
+        self.dt = 1
         self.neo_object = self.data['neo_spikes_dict']
         self.delay = self.get_delay()
-        self.input_layer_name = self.get_ordered_layer_names(self.spikes_dict)[
-            0]
-        self.output_layer_name = self.get_ordered_layer_names(
-            self.spikes_dict)[-1]
+        self.input_layer_name = self.layer_names[0]
+        self.output_layer_name = self.layer_names[-1]
         self.input_layer_shape = (32, 32, 3)
         self.input_spikes = self.spikes_dict[self.input_layer_name]
         self.output_spikes = self.spikes_dict[self.output_layer_name]
-        self.last_but_one_layer = self.spikes_dict[self.get_ordered_layer_names(
-            self.spikes_dict)[-2]]
+        self.last_but_one_layer = self.layer_names[-2]
         self.number_of_examples = self.runtime // self.t_stim
-
+        self.y_pred = self.get_batch_predictions() 
         labels = np.load(
             '/mnt/snntoolbox/snn_toolbox_private/examples/models/05-mobilenet_dwarf_v1/label_names.npz')
         self.label_names = labels['arr_0']
@@ -37,18 +38,10 @@ class OutputDataProcessor():
         self.x_test = x_test_file['arr_0']
         x_test_file.close()
 
-    def get_ordered_layer_names(dictionary):
-        self.layer_names = list(dictionary)
-        layer_names.sort()
-        layer_names[0] = layer_names.pop(-1)
-        return layer_names
-
-    def get_ordered_layer_names_from_data(self):
-        if self.spikes_dict is None:
-            spikes_dict = self.reconstruct_spikes_dict()
-        dictionary = spikes_dict.keys()
-        return self.get_ordered_layer_names(dictionary)
-
+    def order_layer_names(self):
+        self.layer_names.sort()
+        self.layer_names[0] = self.layer_names.pop(-1)
+        
     def get_shape_from_name(name):
         shape_string = name.split('_')
         if len(shape_string) < 2:
@@ -70,13 +63,12 @@ class OutputDataProcessor():
             'sim_time']
         unexpected_files = [
             file for file in self.data.files if file not in expected_files]
-        self.spike_dict = {
-            file_name: self.data[file_name] for file_name in unexpected_files}
+        return {file_name: self.data[file_name] for file_name in unexpected_files}
 
     def get_delay(self):
         'Cannnot currently calculate model with delays other than 1 ms between layers'
 
-        return (self.N_layers - 1) * self.dt
+        return (self.N_layer - 1) * self.dt
 
     def get_bounds(self, bin_number):
         #delay = get_propagation_delay(t_stim, N_layers)
@@ -89,7 +81,7 @@ class OutputDataProcessor():
             print('Final bin cutoff.')'''
         return lower_end_bin_time, higher_end_bin_time
 
-    def get_bin_spikes(self, bin_number):
+    def get_bin_spikes(self, spikes, bin_number):
         lower_end_bin_time, higher_end_bin_time = self.get_bounds(bin_number)
         output = spikes[np.where((spikes[:, 1] >= lower_end_bin_time) & (
             spikes[:, 1] < higher_end_bin_time)), :]
@@ -98,13 +90,13 @@ class OutputDataProcessor():
 
     def get_counts(self, spikes, bin_number):
         min_length = 3 * 32**2
-        spikes = get_bin_spikes(spikes, bin_number)
+        spikes = self.get_bin_spikes(spikes, bin_number)
         just_spikes = spikes.reshape((-1, 2))[:, 0]
-        counts = np.bincount(just_spikes, minlength=minlength)
+        counts = np.bincount(just_spikes, minlength=min_length)
         return counts
 
     def get_rates(self, spikes, bin_number):
-        return get_counts(spikes, bin_number) / self.t_stim
+        return self.get_counts(spikes, bin_number) / self.t_stim
 
     def plot_rates(self, rates):
         shape = shape = (32, 32, 3)
@@ -117,15 +109,26 @@ class OutputDataProcessor():
 
     def get_prediction(self, spikes, bin_number):
         output_size = 10
-        counts = get_counts(spikes, bin_number, t_stim, 10)
+        counts = self.get_counts(spikes, bin_number)
         if counts.max() > 0:
-            return np.argmax(counts)
+            return int(np.argmax(counts))
         else:
             return -1
 
     def get_batch_predictions(self):
         actual_test_labels = self.y_test[:self.number_of_examples].ravel()
-        self.y_pred = np.ones(self.number_of_examples) * (-1)
+        y_pred = np.ones(self.number_of_examples) * (-1)
         for bin_number in range(self.number_of_examples):
-            self.y_pred[bin_number] = get_prediction(
+            y_pred[bin_number] = self.get_prediction(
                 self.output_spikes, bin_number)
+        return y_pred
+
+    def plot_output(self, bin_number):
+        if bin_number > self.number_of_examples: 
+            raise Exception('bin_number greater than number_of_examples')
+            bin_number = self.number_of_examples-1
+        output_spikes = self.get_counts(self.output_spikes, bin_number)
+        output_labels, output_counts = np.unique(output_spikes[:,0], return_counts=True)
+        label_names = [name.decode('utf-8') for name in label_names]
+        plt.bar(output_labels, output_counts)
+        plt.xticks(output_labels, label_names, rotation=90)
