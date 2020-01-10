7df7869db7631d7d13b0e880a9a25a6dfc03a35e
Petrut Bogdan <petrut.bogdan@manchester.ac.uk>
2020-01-10 11:18:05 +0000

    easy to run experiment shell scripts

---
+++ pynn_object_serialisation/experiments/mobilenet_testing/dense_mobilenet_experiment/run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup python ../imagenet_testing.py ../keras_mobilenet --testing_examples 10 2>&1 &
---
+++ pynn_object_serialisation/experiments/mobilenet_testing/sparse_mobilenet_experiment/run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup python ../imagenet_testing.py ../sparse_mobilenet_hard_r --testing_examples 10 2>&1 &
