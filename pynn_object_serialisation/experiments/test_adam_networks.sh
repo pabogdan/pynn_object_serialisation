63cc44046dce10c5d6d5acc1bb3837d3f2ade31b
ej159 <edwardgeorgejones@gmail.com>
2020-05-18 14:48:26 +0100

    Add parallel scripts

---
+++ pynn_object_serialisation/experiments/test_adam_networks.sh
@@ -0,0 +1,20 @@
+nohup python batch_runner.py \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_sparse_hard_adam_run_serialised \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_sparse_hard_adam_no_rew_run_serialised \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_sparse_soft_adam_run_serialised \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_sparse_decay_adam_run_serialised \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_dense_adam_run_serialised \
+--model_script mnist_testing/mnist_testing.py \
+--t_stim 1000 \
+--max_processes 10 > parallel_run_1000_dt_1.0.out 2>&1 &
+
+nohup python batch_runner.py \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_sparse_hard_adam_run_serialised \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_sparse_hard_adam_no_rew_run_serialised \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_sparse_soft_adam_run_serialised \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_sparse_decay_adam_run_serialised \
+./mnist_testing/networks/trained_model_of_lenet_300_100_relu_crossent_dense_adam_run_serialised \
+--model_script mnist_testing/mnist_testing.py \
+--t_stim 500 \
+--no_slices 20 --timestep 0.1 \
+--max_processes 20 > parallel_run_500_dt_0.1.out 2>&1 &
