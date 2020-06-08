903e50f0407447d1ea15aa34280c0fdebd069442
ej159 <edwardgeorgejones@gmail.com>
2020-06-08 13:22:25 +0100

    Fix running of the model on sparse examples

--- pynn_object_serialisation/experiments/mnist_testing/SHAP_experiment.py
+++ pynn_object_serialisation/experiments/mnist_testing/SHAP_experiment.py
@@ -1,19 +1,15 @@
 import argparse
+import tensorflow as tf
 import tensorflow.keras as keras
 #import keras
 from keras_rewiring.sparse_layer import Sparse, SparseConv2D, SparseDepthwiseConv2D
 import shap
 import numpy as np
+np.random.seed(42)
 
 c_obj = {'Sparse': Sparse,
              'SparseConv2D': SparseConv2D,
              'SparseDepthwiseConv2D': SparseDepthwiseConv2D}
-
-
-batch_size = 10
-
-# input image dimensions
-img_rows, img_cols = 28, 28
 
 parser = argparse.ArgumentParser(
     description='SHAP experiment argparser',
@@ -24,7 +20,6 @@ parser.add_argument('model', type=str,
 
 args = parser.parse_args()
 
-
 #Load MNIST dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
@@ -32,12 +27,16 @@ x_test = x_test.reshape((-1,1, 784))
 x_train = x_train.reshape((-1,1, 784))
 y_test = keras.utils.to_categorical(y_test, 10)
 y_train = keras.utils.to_categorical(y_train, 10)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
 
 #Load an ANN model
-
-model = keras.models.load_model(args.model, compile=False, custom_objects=c_obj)
+model = keras.models.load_model(args.model, compile=True, custom_objects=c_obj)
 
 def change_batch_size(model, batch_size, c_obj):
+    #This is the easiest way of changing batch_size apparently...
     config = model.get_config()
 
     for layer in config['layers']:
@@ -53,15 +52,14 @@ def change_batch_size(model, batch_size, c_obj):
         except:
             print("Could not transfer weights for layer {}".format(layer.name))
 
-    #new_model.layers[-1].activation = activations.softmax
     # rebuild model architecture by exporting and importing via json
-    #new_model = keras.models.model_from_json(model.to_json())
+    #new_model = keras.models.model_from_json(model.to_json(), custom_objects=c_obj)
 
 
 
     return new_model
 
-model = change_batch_size(model, None, c_obj)
+#model = change_batch_size(model, None, c_obj)
 
 
 model.compile(loss=keras.losses.categorical_crossentropy,
@@ -79,40 +77,34 @@ model.summary()
 
 
 
-
-# if K.image_data_format() == 'channels_first':
-#     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-#     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-#     input_shape = (1, img_rows, img_cols)
-# else:
-#     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-#     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-#     input_shape = (img_rows, img_cols, 1)
-#
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-
 score = model.evaluate(x_test, y_test, verbose=0, batch_size=100)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+print(np.argmax(model.predict(x_test[0:10]), axis=1))
+
+
 
 
 #Do the SHAP analysis
 #Taken from shap github
 # select a set of background examples to take an expectation over
-background = x_train[np.random.choice(x_train.shape[0], 100, replace=False)].reshape((-1,1,784))
+
+background = x_train[np.random.choice(x_train.shape[0], 100, replace=False)].reshape((-1, 1, 784))
 
 # explain predictions of the model on four images
 
 e = shap.DeepExplainer(model, background)
 
+index = 9
+
 shap_values = e.shap_values(x_test[0:10], check_additivity=True)
 reshaped_shap_values = [example.reshape((-1, 28, 28)) for example in shap_values]
-# plot the feature attributions
+#Making meaningful labels
 labels = np.expand_dims(np.arange(0,10), axis=0)
 empty = np.repeat(np.zeros_like(labels, dtype=str),9, axis=0)
 empty[:] = ''
 labels = np.vstack((labels, empty))
+
+# plot the feature attributions
 shap.image_plot(reshaped_shap_values, np.array(-x_test[0:10]).reshape((-1, 28, 28)), labels=labels)
