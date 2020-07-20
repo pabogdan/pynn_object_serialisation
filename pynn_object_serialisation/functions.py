Fix scaling values

pynn_object_serialisation/functions.py
@@ -246,16 +246,16 @@ def restore_simulator_from_file(sim, filename, prune_level=1.,
                                        prune_level=prune_level)
 
         #Basically just divide it by 5. I'm sure that will make it work...
-
+        #TODO Make this more rigorous and based on variables
         tau_syn_E = 0.02
         tau_syn_I = 0.02
         # just to give a sensible answer if tau_syn_E and I are different
         t = 1
         tau = (tau_syn_E + tau_syn_I) / 2
-        scale = 10 * t / (tau * (np.exp(-(t / tau)) + 1))
+        scale = 0.1 * t / (tau * (np.exp(-(t / tau)) + 1))
         print('Weights scaled by a factor of {0}'.format(1/scale, ))
 
-        _conn = utils._scale_and_cast_weights(_conn, 1/scale)
+        #_conn = utils._scale_and_cast_weights(_conn, 1/scale)
 
         # build synapse dynamics
         synapse_dynamics = utils._build_synapse_info(sim, proj_info, timestep)